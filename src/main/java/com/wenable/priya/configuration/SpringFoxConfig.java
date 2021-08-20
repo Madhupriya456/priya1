56f47f8df5a71525036b7ff6a6cf3d59cad331e4
Madhu Priya <priya.kotapalli@wenable.com>
2021-08-20 17:12:19 +0530

    first commit

--- src/main/java/com/wenable/priya/configuration/SpringFoxConfig.java
+++ src/main/java/com/wenable/priya/configuration/SpringFoxConfig.java
@@ -1,10 +1,13 @@
 package com.wenable.priya.configuration;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,4 +24,6 @@ public class SpringFoxConfig {
           .paths(PathSelectors.any())                          
           .build();                                           
     }
+	
+	
 }
